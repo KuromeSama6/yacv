bd207522e6e7d20b36580226bd3ea5db276c6930
6ch <kuromesama6@gmail.com>
2024-12-05 13:01:53 -0500

    updated notice on CopyManga downage

--- src/components/views/HomeView.vue
+++ src/components/views/HomeView.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import { BContainer, BRow, BModal } from "bootstrap-vue-next";
+import { useAccountStore } from "@/store";
+import { BContainer, BRow } from "bootstrap-vue-next";
+import { ref } from "vue";
 import { useI18n } from "vue-i18n";
 import { useRouter } from "vue-router";
-import { ref } from "vue";
 import MangaSearchBar from "../home/MangaSearchBar.vue";
-import { useAccountStore } from "@/store";
 const { t } = useI18n();
 const router = useRouter();
 const account = useAccountStore();
@@ -27,44 +27,18 @@ const modal = ref(false);
             <BCardText>
                 {{ t("home.not_logged_in_rationale") }}
             </BCardText>
-            <BButton variant="outline-primary" @click="router.push('/account')">{{
-                t("home.log_in_button")
-            }}</BButton>
+            <BButton variant="outline-primary" @click="router.push('/account')">{{ t("home.log_in_button") }}</BButton>
             <template #footer> {{ t("home.affiliation_disclaimer") }} </template>
         </BCard>
 
-        <BCard
-            class="card"
-            v-if="account.IsLoggedIn()"
-            border-variant="primary"
-            :title="t('home.account_card.lets_get_reading', [account.info?.username])"
-        >
+        <BCard class="card" v-if="account.IsLoggedIn()" border-variant="primary" :title="t('home.account_card.lets_get_reading', [account.info?.username])">
             <div class=""></div>
-            <BButton
-                class="operation-btn"
-                variant="outline-primary"
-                @click="router.push('/account')"
-                >{{ t("home.account_card.button.bookshelf") }}</BButton
-            >
-            <BButton
-                class="operation-btn"
-                variant="outline-primary"
-                @click="router.push('/history')"
-                >{{ t("home.account_card.button.history") }}</BButton
-            >
+            <BButton class="operation-btn" variant="outline-primary" @click="router.push('/account')">{{ t("home.account_card.button.bookshelf") }}</BButton>
+            <BButton class="operation-btn" variant="outline-primary" @click="router.push('/history')">{{ t("home.account_card.button.history") }}</BButton>
         </BCard>
-        <BCard
-            class="card"
-            border-variant="info"
-            align="start"
-            :title="t('home.updates_card.title')"
-            footer-border-variant="info"
-        >
-            <h6>Manga reader navbar rework</h6>
-            <BCardText>
-                The navbar in the manga reader has been reworked. An option to star (add a title to
-                your account bookshelf) has been added.
-            </BCardText>
+        <BCard class="card" border-variant="danger" align="start" :title="t('home.updates_card.title')" footer-border-variant="info">
+            <h6>CopyManga Downage & Rework In Progress</h6>
+            <BCardText> It has been recently confirmed that CopyManga has removed most if not all of its titles due to possible copyright infringements and DMCA takedowns. I have realised that relying on a single source is not a viable solution. I am in the progress of reworking the app to support multiple sources, which will be released in a different Github repository. Stay tuned for more updates. This site will still be available before the new version is released, but the Github repository will be archived. Thank you for your support. </BCardText>
             <template #footer>
                 {{ t("home.updates_card.check_github") }}
                 <a href="https://github.com/KuromeSama6/yacv" target="_blank">Github</a>.
--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,11 @@
-import { createMemoryHistory, createRouter } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
+import AccountView from "./components/views/AccountView.vue";
+import HistoryView from "./components/views/HistoryView.vue";
 import HomeView from "./components/views/HomeView.vue";
 import MangaDetailsView from "./components/views/MangaDetailsView.vue";
 import MangaReader from "./components/views/MangaReader.vue";
-import Cookies from "js-cookie";
 import OptionsView from "./components/views/OptionsView.vue";
-import HistoryView from "./components/views/HistoryView.vue";
-import AccountView from "./components/views/AccountView.vue";
 
 const routes = [
     { path: "/", component: HomeView },
@@ -16,18 +15,18 @@ const routes = [
     {
         path: "/details/:mangaId",
         component: MangaDetailsView,
-        props: true
+        props: true,
     },
     {
         path: "/read/:mangaId/:chapterId",
         component: MangaReader,
-        props: true
-    }
+        props: true,
+    },
 ];
 
 const router = createRouter({
-    history: createMemoryHistory(),
-    routes
+    history: createWebHashHistory(),
+    routes,
 });
 
 export default router;
